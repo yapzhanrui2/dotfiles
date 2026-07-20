20c9a0fa3b38d5d37614b184699243e30a5258c6
Ray Yap <rayyap.dev@gmail.com>
2026-07-20 19:23:48 +1000

    init twice fix

--- home.nix
+++ home.nix
@@ -23,30 +23,29 @@ in
 
   programs.zsh = {
     enable = true;
+    enableCompletion = false;
 
     shellAliases = {
       go-server = "ssh ray@ray-homelab-ubuntu";
-      zj = "zellij";
-    };
+     zj = "zellij";
+   };
 
-    initContent = ''
-      # Zim framework
+   initContent = ''
       ZIM_HOME="''${ZDOTDIR:-$HOME}/.zim"
 
-      if [[ ! "$ZIM_HOME/init.zsh" -nt "''${ZIM_CONFIG_FILE:-''${ZDOTDIR:-$HOME}/.zimrc}" ]]; then
+     if [[ ! "$ZIM_HOME/init.zsh" -nt "''${ZIM_CONFIG_FILE:-''${ZDOTDIR:-$HOME}/.zimrc}" ]]; then
         source /opt/homebrew/opt/zimfw/share/zimfw.zsh init
       fi
 
-      source "$ZIM_HOME/init.zsh"
+     source "$ZIM_HOME/init.zsh"
 
-      # Ghostty shell integration
       if [[ -n "$GHOSTTY_RESOURCES_DIR" ]] &&
-         [[ -r "$GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration" ]]; then
-        source "$GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration"
-      fi
+        [[ -r "$GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration" ]]; then
+       source "$GHOSTTY_RESOURCES_DIR/shell-integration/zsh/ghostty-integration"
+    fi
 
-      unsetopt extended_glob
-    '';
+     unsetopt extended_glob
+   '';
   };
 
   programs.fzf = {
