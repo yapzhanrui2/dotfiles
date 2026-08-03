4d1dd9cbb7208ee9c7918ce683f35986585135bf
Ray Yap <rayyap.dev@gmail.com>
2026-08-03 23:48:06 +1000

    Uv Path

--- home.nix
+++ home.nix
@@ -33,6 +33,7 @@ in
    initContent = ''
      export BUN_INSTALL="$HOME/.bun"
      export PATH="$BUN_INSTALL/bin:$PATH"
+     export PATH="$HOME/.local/bin:$PATH"
 
      ZIM_HOME="''${ZDOTDIR:-$HOME}/.zim"
 
